bfefa755550b9725cc6aece3726f85d38ab1f318
Aniket Shinde <aniket.shinde.co@gmail.com>
2023-09-14 16:49:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ import re
 app = Flask(__name__)
 
 
-app.secret_key = '12345'
+app.secret_key = '1234'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12345'
-app.config['MYSQL_DB'] = 'project'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'ved'
 
 mysql = MySQL(app)
 
@@ -26,7 +26,7 @@ def login():
 		username = request.form['username']
 		password = request.form['password']
 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+		cursor.execute('SELECT * FROM register WHERE username = % s AND password = % s', (username, password, ))
 		account = cursor.fetchone()
 		if account:
 			session['loggedin'] = True
@@ -52,8 +52,11 @@ def register():
 		username = request.form['username']
 		password = request.form['password']
 		email = request.form['email']
+		branch = request.form['branch']
+		year = request.form['year']
+		subject = request.form['subject']
 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+		cursor.execute('SELECT * FROM register WHERE username = % s', (username, ))
 		account = cursor.fetchone()
 		if account:
 			msg = 'Account already exists !'
@@ -64,7 +67,7 @@ def register():
 		elif not username or not password or not email:
 			msg = 'Please fill out the form !'
 		else:
-			cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+			cursor.execute('INSERT INTO register VALUES (NULL, % s, % s, % s, % s, % s, % s)', (username, email, branch,year, subject,password))
 			mysql.connection.commit()
 			msg = 'You have successfully registered !'
 	elif request.method == 'POST':
